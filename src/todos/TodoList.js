3bd853d9b540f2afa790edc9e411b20839fc86e7
Nicolae Florin Munteanu <1899198@crosemont.qc.ca>
2021-10-09 07:45:34 -0400

    Adds todo components and styles

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './TodoList.css';
+import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 
-const TodoList = ({ todos }) => (
+
+const TodoList = ({ todos = [{ text: 'Hello' }] }) => (
     <div className="list-wrapper">
+        <NewTodoForm />
         {todos.map(todo => <TodoListItem todo={todo} />)}
     </div>
 );
